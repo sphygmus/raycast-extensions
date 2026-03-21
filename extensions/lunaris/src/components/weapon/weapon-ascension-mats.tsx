5b41abc5df01ada01768779d1c3c1dfa3ac645db
Sphygmus <kemalcangedik@mail.com>
2026-03-22 01:55:02 +0300

    Add lunaris extension

---
+++ extensions/lunaris/src/components/weapon/weapon-ascension-mats.tsx
@@ -0,0 +1,77 @@
+import { List } from "@raycast/api";
+import { usePromise } from "@raycast/utils";
+import { getGameVersion, getMaterials } from "@/lib/utils/lunaris";
+
+type Props = { id: string };
+
+export default function WeaponAscensionMaterials({ id }: Props) {
+  const { isLoading: loadingWeapon, data: weapon } = usePromise(async () => {
+    const gameVersion = await getGameVersion();
+    if (!gameVersion) return undefined;
+
+    const res = await fetch(
+      `https://api.lunaris.moe/data/${gameVersion}/en/weapon/${id}.json`,
+    );
+    const characterData = (await res.json()) as GenshinWeaponDetail;
+    return characterData;
+  });
+
+  const { isLoading, data: materials } = usePromise(getMaterials);
+
+  if (!weapon) return;
+
+  const materialList = weapon.ascension.map(({ count, icon }) => {
+    const materialID = icon.split("_").at(-1);
+    if (!materials || !materialID || isNaN(Number(materialID)))
+      return {
+        id: "-1",
+        name: "???",
+        description: "???",
+        amount: 0,
+        icon: "",
+      };
+
+    return {
+      id: materialID,
+      name: materials[materialID].enName,
+      description: materials[materialID].enDescription,
+      amount: count,
+      icon: `https://api.lunaris.moe/data/assets/items/${icon}.webp`,
+    };
+  });
+
+  return (
+    <List
+      isLoading={isLoading || loadingWeapon}
+      navigationTitle={`${weapon.name} / Materials`}
+      isShowingDetail
+    >
+      {materialList.map(
+        (item, index) =>
+          item && (
+            <List.Item
+              key={item.id === "-1" ? index : item.id}
+              title={item.name || `ID: ${item.id}`}
+              subtitle={`x${item.amount.toLocaleString()}`}
+              keywords={[item.id, item.name]}
+              icon={{ source: item.icon }}
+              detail={
+                <List.Item.Detail
+                  markdown={`
+![](${item.icon})
+# ${item.name || "Unknown Item"} (x${item.amount.toLocaleString()})
+${item.description
+  .replace(/<color=[^>]+>(.*?)<\/color>/gi, "**$1**")
+  .replace(/\{LINK#[^}]+\}(.*?)\{\/LINK\}/gi, "_$1_")
+  .replace(/\\n/g, "\n\n")
+  .replace(/<[^>]*>/g, "")
+  .trim()}
+		  `}
+                />
+              }
+            />
+          ),
+      )}
+    </List>
+  );
+}
---
+++ extensions/lunaris/src/list-characters.tsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { getPreferenceValues, Grid } from "@raycast/api";
+import { useCachedState, usePromise } from "@raycast/utils";
+
+import { getAllCharacters } from "@/lib/utils/lunaris";
+import { WEAPON_TYPE } from "@/lib/constants";
+
+import CharacterGridItem from "@/components/character/character-grid-item";
+
+export default function Command() {
+  const { isLoading, data: characters } = usePromise(getAllCharacters);
+  const [pinned, setPinned] = useCachedState<string[]>("pinned_characters", []);
+  const preferences = getPreferenceValues<Preferences>();
+  const [filter, setFilter] = useState<string>("All");
+
+  return (
+    <Grid
+      columns={7}
+      isLoading={isLoading}
+      inset={Grid.Inset.Zero}
+      searchBarAccessory={
+        <Grid.Dropdown
+          tooltip="Filter characters"
+          onChange={(newValue) => setFilter(newValue)}
+        >
+          <Grid.Dropdown.Item key={"All"} title={"All"} value={"All"} />
+          <Grid.Dropdown.Section title="Elements">
+            {["Pyro", "Hydro", "Electro", "Cryo", "Anemo", "Geo", "Dendro"].map(
+              (type) => (
+                <Grid.Dropdown.Item
+                  key={type}
+                  title={type}
+                  value={type}
+                  icon={`elements/${type.toLowerCase()}.png`}
+                />
+              ),
+            )}
+          </Grid.Dropdown.Section>
+          <Grid.Dropdown.Section title="Weapons">
+            {Object.keys(WEAPON_TYPE).map((type) => {
+              const key = type as keyof typeof WEAPON_TYPE;
+              return (
+                <Grid.Dropdown.Item
+                  key={key}
+                  title={WEAPON_TYPE[key]}
+                  value={key}
+                  icon={`weapons/${type}.png`}
+                />
+              );
+            })}
+          </Grid.Dropdown.Section>
+        </Grid.Dropdown>
+      }
+    >
+      <Grid.Section title="Pinned Characters">
+        {characters &&
+          Object.entries(characters)
+            .reverse()
+            .filter(([id]) => {
+              return pinned.includes(id);
+            })
+            .map(([id, char]) => (
+              <CharacterGridItem
+                key={id}
+                id={id}
+                character={char}
+                pinned={pinned}
+                setPinned={setPinned}
+              />
+            ))}
+      </Grid.Section>
+      <Grid.Section
+        title={pinned && pinned.length > 0 ? "All Characters" : undefined}
+      >
+        {characters &&
+          Object.entries(characters)
+            .reverse()
+            .filter(([id, char]) => {
+              const releaseTimeMs = char.releaseDate * 1000;
+
+              const isAllowed =
+                (preferences.allowUnreleased || releaseTimeMs <= Date.now()) &&
+                !char.enName.startsWith("Manekin") &&
+                char.enName !== "Traveler";
+
+              const matchesFilter =
+                !filter ||
+                filter === "All" ||
+                char.element === filter ||
+                char.weaponType === filter;
+
+              return isAllowed && matchesFilter && !pinned.includes(id);
+            })
+            .map(([id, char]) => (
+              <CharacterGridItem
+                key={id}
+                id={id}
+                character={char}
+                pinned={pinned}
+                setPinned={setPinned}
+              />
+            ))}
+      </Grid.Section>
+    </Grid>
+  );
+}
